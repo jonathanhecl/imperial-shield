f6eb18eb00d30a2f8bf2ae47d0d405ef82985f8b
Jonathan Hecl <jonathanhecl@gmail.com>
2026-02-01 00:55:27 -0300

    feat: Add DashboardWindow, implementing a new command center UI with RPG-themed styling.

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 title Imperial Shield - Run
-cd /d "%~dp0ImperialShield\bin\Release\net8.0-windows"
+cd /d "%~dp0ImperialShield\bin\Debug\net8.0-windows"
 
 echo Iniciando Imperial Shield...
 start "" "ImperialShield.exe"
